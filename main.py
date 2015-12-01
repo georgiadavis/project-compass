dec13f5c65f3afd6ab2f322161542d67f28e58fe
Anthony Fumagalli <anthony.fumagalli@udacity.com>
2015-12-01 12:02:58 -0800

    Finished refactor into GAE webapp

--- main.py
+++ main.py
@@ -14,11 +14,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import os
+import webapp2
+import jinja2
+from google.appengine.api import users
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(template_dir),
+    extensions = ['jinja2.ext.autoescape'],
+    autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, template_values):
+        t = jinja_env.get_template(template)
+        return t.render(template_values)
+
+    def render(self, template, template_values):
+        self.write(self.render_str(template, template_values))
+
+    def get_user_info(self):
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+            user_name = user.nickname()
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login with Google'
+            user_name = 'Anonymous'
+        return url, url_linktext, user_name
+
+class MainHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        url, url_linktext, user_name = self.get_user_info()
+        if user_name == 'anthony.fumagalli@knowlabs.com':
+            admin = True
+        else:
+            admin = False
+        template_values = {
+            'user_name': user_name,
+            'url': url,
+            'url_linktext': url_linktext,
+            'admin': admin
+        }
+        self.render('index.html', template_values)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
