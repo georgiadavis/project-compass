db4fe82cc3f877a8e6f8cd309bb7ea11570d06c9
Anthony Fumagalli <anthony.fumagalli@udacity.com>
2015-12-01 17:48:47 -0800

    Added ability for admin to upload photos to Datastore

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
 import os
+import cgi
+import urllib
 import webapp2
 import jinja2
+from google.appengine.api import images
 from google.appengine.api import users
+from google.appengine.ext import ndb
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(template_dir),
     extensions = ['jinja2.ext.autoescape'],
     autoescape = True)
+
+DEFAULT_MAP_NAME = 'default'
+
+def map_key(map_name=DEFAULT_MAP_NAME):
+    """Constructs Datastore key for PhotoMap entity."""
+    return ndb.Key('PhotoMap', map_name)
+
+class Author(ndb.Model):
+    """Sub model for representing an author."""
+    identity = ndb.StringProperty(indexed = True)
+    name = ndb.StringProperty(indexed = False)
+    email = ndb.StringProperty(indexed = False)
+
+class Photo(ndb.Model):
+    """Models a PhotoMap entry with an author, image, location, description, and date"""
+    author = ndb.StructuredProperty(Author)
+    image = ndb.BlobProperty()
+    location = ndb.StringProperty()
+    description = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -57,10 +66,26 @@ class Handler(webapp2.RequestHandler):
             admin = False
         return url, url_linktext, user_name, admin
 
-class MainHandler(Handler):
+class Image(Handler):
     def get(self):
+        photo_key = ndb.Key(urlsafe=self.request.get('img_id'))
+        photo = photo_key.get()
+        if photo.image:
+            self.response.headers['Content-Type'] = 'image/png'
+            self.response.out.write(photo.image)
+        else:
+            self.response.out.write('No image')
+
+class MainPage(Handler):
+    def get(self):
+        map_name = self.request.get('map_name', DEFAULT_MAP_NAME)
         url, url_linktext, user_name, admin = self.get_user_info()
+        photos_query = Photo.query(
+            ancestor = map_key(map_name)).order(-Photo.date)
+        photos = photos_query.fetch()
+
         template_values = {
+            'map_name': map_name,
             'user_name': user_name,
             'url': url,
             'url_linktext': url_linktext,
@@ -68,6 +93,25 @@ class MainHandler(Handler):
         }
         self.render('index.html', template_values)
 
+class PhotoMap(Handler):
+    def post(self):
+        map_name = self.request.get('map_name', DEFAULT_MAP_NAME)
+        photo = Photo(parent = map_key(map_name))
+        user = users.get_current_user()
+        if user:
+            photo.author = Author(
+                identity = user.user_id(),
+                name = user.nickname(),
+                email = user.email())
+        photo.image = self.request.get('img')
+        photo.location = self.request.get('location')
+        photo.description = self.request.get('description')
+        photo.put()
+        self.redirect('/?' + urllib.urlencode(
+            {'map_name': map_name}))
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    ('/img', Image),
+    ('/post', PhotoMap)
 ], debug=True)
